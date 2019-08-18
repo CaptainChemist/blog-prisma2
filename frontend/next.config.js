f93b2823ccb7b3a7ee9b606fd673ceac436378ca
Stephen Jensen <stephen.jensen3@gmail.com>
2019-08-18 16:17:12 -0500

    added hot-module-reload

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,5 +4,13 @@ module.exports = withCSS({
   target: 'serverless',
   env: {
     BACKEND_URL: process.env.BACKEND_URL
+  },
+  webpackDevMiddleware: (config) => {
+    // Solve compiling problem via vagrant
+    config.watchOptions = {
+      poll: 1000,   // Check for changes every second
+      aggregateTimeout: 300,   // delay before rebuilding
+    };
+    return config;
   }
 });
